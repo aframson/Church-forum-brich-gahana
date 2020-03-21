86ef273b97a57212f6acdae05bd3a28b0a561ee7
aframson <obiriaframson@gmail.com>
2020-03-21 17:09:03 +0000

    first phase of the project

--- Church/App.js
+++ Church/App.js
@@ -1,19 +1,143 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {Component} from 'react';
+import {validateAll} from 'indicative/validator';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import {Hoshi} from 'react-native-textinput-effects';
+// import Axios from 'axios';
+class Register extends Component {
 
-export default function App() {
+
+  state = {
+    fname:'',
+    lname:'',
+    uname:'',
+    password:'',
+    cpassword:'',
+    phone:''
+  }
+
+
+
+  RegisterUser = async(data) =>{
+
+
+
+    const rules  ={
+
+          fname:'required|string',
+          lname:'required|string',
+          uname:'required|string',
+          password:'required|string|min:6|confirmed',
+          phone:'required|string'
+
+    }
+
+  const message ={
+         required:(field) => `${field} is required`,
+         'password.confirmed':'the passwords do not match',
+         'password.min':'password is too short'
+  }
+
+
+     try {
+      
+        await validateAll(data,rules,message)
+
+
+
+
+     } catch (error) {
+       
+     }
+
+
+  }
+  
+
+
+
+
+
+
+
+
+
+
+  render(){
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Refister</Text>
+
+      <Hoshi 
+      label="First name" 
+      value={this.state.fname} 
+      onChangeText={fname => this.setState({fname})} 
+      backgroundColor={"#fff"}
+      borderColor={"#b76c94"}
+      borderHeight={3}
+      inputPadding={16}
+      />
+       <Hoshi 
+      label="Last Name" 
+      value={this.state.lname} 
+      onChangeText={lname => this.setState({lname})} 
+      backgroundColor={"#fff"}
+      borderColor={"#b76c94"}
+      borderHeight={3}
+      inputPadding={16}
+      />
+       <Hoshi 
+      label="User Name" 
+      value={this.state.uname} 
+      onChangeText={uname => this.setState({uname})} 
+      backgroundColor={"#fff"}
+      borderColor={"#b76c94"}
+      borderHeight={3}
+      inputPadding={16}
+      />
+       <Hoshi 
+      label="Password" 
+      value={this.state.passowrd} 
+      secureTextEntry
+      onChangeText={passowrd => this.setState({passowrd})} 
+      backgroundColor={"#fff"}
+      borderColor={"#b76c94"}
+      borderHeight={3}
+      inputPadding={16}
+      />
+      <Hoshi 
+      label="Confirm Password" 
+      value={this.state.cpassowrd} 
+      secureTextEntry
+      onChangeText={cpassowrd => this.setState({cpassowrd})} 
+      backgroundColor={"#fff"}
+      borderColor={"#b76c94"}
+      borderHeight={3}
+      inputPadding={16}
+      />
+       <Hoshi 
+      label="Telephone" 
+      value={this.state.phone} 
+      onChangeText={phone => this.setState({phone})} 
+      backgroundColor={"#fff"}
+      borderColor={"#b76c94"}
+      borderHeight={3}
+      inputPadding={16}
+      style={{marginButtom:45}}
+      />
+
+     <Button title="Register" onPress={() => this.RegisterUser(this.state)} />
     </View>
   );
+
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     padding:10
   },
 });
+
+
+export default Register;
